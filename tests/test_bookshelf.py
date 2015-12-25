f4709d8ea4b2b02f0619cb47ae887418c7e4d649
Alireza Bashiri <azbshiri@gmail.com>
2015-12-25 17:31:26 +0330

    Rename test functions
    * Let's make names more readable

--- tests/test_bookshelf.py
+++ tests/test_bookshelf.py
@@ -6,17 +6,17 @@ from lxml.html import HtmlElement
 import requests
 
 
-def test_that_it_gets_a_list_of_authors():
+def test_that_it_returns_a_list_of_authors():
     authors = get_authors_with_names_that_start_with('M')
     assert isinstance(authors, list)
 
 
-def test_that_it_gets_a_list_of_books():
+def test_that_it_returns_a_list_of_books():
     books = search_for_books_by_author('Damian Marrett')
     assert isinstance(books, list)
 
-def test_that_it_creates_html_element_from_response():
+
+def test_that_it_returns_an_html_element():
     URL = 'https://en.wikipedia.org/wiki/List_of_authors_by_name:_M'
     response = requests.get(URL)
     assert isinstance(create_html_element_from(response), HtmlElement)
-
